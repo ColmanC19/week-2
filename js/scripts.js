dc858b99916e1e91d0128467fb4192ddefb4ed4e
Colman Currie <curriecolman@gmail.com>
2019-09-17 20:31:42 -0700

    Added radio box, submit button, more css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+  var name = prompt("Why hello, can I have your first name");
+}
+
 var geography = parseInt("#geography").val();
 var weather = parseInt("#weather").val();
 var activities = parseInt("#activities").val();
